68d2fb36092932432f189157ac0eab471365e0fa
Andrei Bursuc <andrei.bursuc@sumup.com>
2024-04-17 15:55:46 +0300

    Create the project

---
+++ AJFVaslui/AJFVaslui/AJFVasluiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AJFVasluiApp.swift
+//  AJFVaslui
+//
+//  Created by Andrei-Stefan BURSUC on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AJFVasluiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
